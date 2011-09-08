e3cb6ec77b3c964564bd72d4bcbb863f38079f52
Robert Thau <rst@ai.mit.edu>
2011-09-08 17:18:41 -0400

    Initial commit, from g8.

---
+++ project/build.scala
@@ -0,0 +1,29 @@
+import sbt._
+
+import Keys._
+import AndroidKeys._
+
+object General {
+  val settings = Defaults.defaultSettings ++ Seq (
+    version := "0.1",
+    scalaVersion := "2.9.0-1",
+    platformName in Android := "android-7"
+  )
+
+  lazy val fullAndroidSettings =
+    General.settings ++
+    AndroidProject.androidSettings ++
+    TypedResources.settings ++
+    AndroidMarketPublish.settings ++ Seq (
+      keyalias in Android := "change-me",
+      libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+    )
+}
+
+object AndroidBuild extends Build {
+  lazy val main = Project (
+    "Positronic Net Tutorial Todo",
+    file("."),
+    settings = General.fullAndroidSettings
+  )
+}
