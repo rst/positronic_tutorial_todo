b6dc16df15f8c3e203fd170ea2c654736ec14e3a
Robert Thau <rst@ai.mit.edu>
2011-09-25 19:27:13 -0400

    Properly handle 'enter' in the EditText.

--- src/main/scala/Todo.scala
+++ src/main/scala/Todo.scala
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 import android.view.View
 import android.view.View.OnClickListener
+import android.view.KeyEvent
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ListView
@@ -59,7 +60,10 @@ class TodoItemsActivity
       TodoItem ! Delete( adapter.getItem( posn ))
     }
 
-    findView( TR.addButton ).onClick {
+    findView( TR.addButton ).onClick { addItem }
+    findView( TR.newItemText ).onKey( KeyEvent.KEYCODE_ENTER ){ addItem }
+
+    def addItem = {
       val text = findView( TR.newItemText ).getText.toString.trim
       if (text != "") {
         TodoItem ! Save( new TodoItem( text ))
