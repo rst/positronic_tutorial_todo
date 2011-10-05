fe5e6ad51c45c3130db9b1da4dc0b5e546b8bc85
Robert Thau <rst@ai.mit.edu>
2011-10-04 22:27:57 -0400

    Add "deleted" status for todo items, and machinery to display it.

--- src/main/scala/Todo.scala
+++ src/main/scala/Todo.scala
@@ -3,6 +3,8 @@ package org.positronicnet.tutorial.todo
 import android.app.Activity
 import android.os.Bundle
 
+import android.content.Context
+import android.util.AttributeSet
 import android.view.View
 import android.view.View.OnClickListener
 import android.view.KeyEvent
@@ -11,6 +13,7 @@ import android.widget.AdapterView.OnItemClickListener
 import android.widget.ListView
 import android.widget.Button
 import android.widget.TextView
+import android.graphics.Paint
 
 import org.positronicnet.db.Database
 import org.positronicnet.orm.RecordManager
@@ -28,19 +31,43 @@ object TodoDb extends Database( filename = "todos.sqlite3" )
                _id integer primary key,
                description string
              )
-         """)
+         """,
+         """ alter table todo_items add column is_done integer default 0 """
+       )
 }
 
 case class TodoItem( description: String = null, 
+                     isDone: Boolean     = false,
                      id: Long            = ManagedRecord.unsavedId 
                    )
   extends ManagedRecord( TodoItem )
 {
   def setDescription( s: String ) = this.copy( description = s )
+  def toggleDone = this.copy( isDone = !isDone )
   override def toString = this.description
 }
 
 object TodoItem extends RecordManager[ TodoItem ]( TodoDb( "todo_items" ))
+
+class TodoItemsAdapter( activity: PositronicActivityHelpers )
+  extends IndexedSeqSourceAdapter[ TodoItem ]( 
+    activity, TodoItem, itemViewResourceId = R.layout.todo_item )
+{
+  override def bindView( view: View, it: TodoItem ) =
+    view.asInstanceOf[ TodoItemView ].setTodoItem( it )
+}
+
+class TodoItemView( context: Context, attrs: AttributeSet = null )
+ extends TextView( context, attrs ) 
+{
+   def setTodoItem( item: TodoItem ) = {
+     setText( item.description )
+     setPaintFlags( 
+       if (item.isDone) getPaintFlags | Paint.STRIKE_THRU_TEXT_FLAG 
+       else getPaintFlags & ~Paint.STRIKE_THRU_TEXT_FLAG
+     )
+   }
+}
 
 class TodoItemsActivity 
   extends Activity with PositronicActivityHelpers with ViewHolder
@@ -49,17 +76,12 @@ class TodoItemsActivity
     setContentView( R.layout.todo_items )
     useAppFacility( TodoDb )
 
-    val adapter: IndexedSeqSourceAdapter[ TodoItem ] = 
-      new IndexedSeqSourceAdapter(
-        this, TodoItem,
-        itemViewResourceId = android.R.layout.simple_list_item_1 )
-  
+    val adapter = new TodoItemsAdapter( this )
     findView( TR.listItemsView ).setAdapter( adapter )
 
     findView( TR.listItemsView ).onItemClick{ (view, posn, id) =>
-      TodoItem ! Delete( adapter.getItem( posn ))
+      TodoItem ! Save( adapter.getItem( posn ).toggleDone )
     }
-
     findView( TR.addButton ).onClick { addItem }
     findView( TR.newItemText ).onKey( KeyEvent.KEYCODE_ENTER ){ addItem }
 
